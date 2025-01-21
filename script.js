2c64d321a7de8f5730ffcb5afc1efca556632125
Hreeth Gupta <hreethgupta2006@gmail.com>
2025-01-22 02:11:58 +0530

    button fix #3

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function copy() {
-    const btn = document.querySelector('.copy-btn')
+const btn = document.querySelector('.copy-btn')
+btn.addEventListener('click', copy)
+
+function copy(e) {
     const walletElem = document.querySelector('.wallet-text')
     const walletAddress = walletElem.textContent.trim()
     navigator.clipboard.writeText(walletAddress)
